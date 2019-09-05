daac7a848028d3edd82996651d9536b196d873a4
Cristiano Faustino <cristiadu@gmail.com>
2019-09-05 00:41:57 -0700

    stateless class to functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-    static defaultProps = {
-        title: "Navbar",
-        icon: "fad fa-blog"
-    };
+const Navbar = ({ icon, title }) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    );
+};
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
+Navbar.defaultProps = {
+    title: "Navbar",
+    icon: "fad fa-blog"
+};
 
-    render() {
-        const {icon, title} = this.props;
-
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={icon} /> {title}
-                </h1>
-            </nav>
-        );
-    }
-}
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
